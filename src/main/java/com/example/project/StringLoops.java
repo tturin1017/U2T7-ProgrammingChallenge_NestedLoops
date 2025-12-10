0c7557866d13fefb97667a6ab81361c3b453e735
Tatiana Turin <146445767+tturin1017@users.noreply.github.com>
2025-12-10 08:56:23 -0500

    Initial commit

---
+++ src/main/java/com/example/project/StringLoops.java
@@ -0,0 +1,69 @@
+package com.example.project;
+
+public class StringLoops {
+
+   public StringLoops() { }
+
+   //WARM UP 
+    // using a for loop remove all 'a' characters from input string
+   public String removeA(String str) {
+       return "";
+   }
+
+
+   // WARM UP
+   // using a while loop remove all 'a' characters from input string
+   public String removeA2(String str) {
+       return "";
+   }
+
+
+   //WARM UP 
+   //Reverse a string by counting down from the end to the start
+   public String reverseString(String str) {
+       return ""
+   }
+
+
+   //WARM UP
+   //Reverse a string by counting up from the start to the end
+   public String reverseString2(String str) {
+       return "";
+   }
+
+
+   // using a for loop
+   public String replaceCharacterV1(String searchChar, String origStr, String replaceChar){
+        return "";
+   }
+
+
+   // using a while loop
+   public String replaceCharacterV2(String searchChar, String origStr, String replaceChar) {
+       return "";
+   }
+
+   public int countString(String searchString, String origString) {
+       return 0;
+   }
+
+   public String removeString(String searchString, String origString) {
+       return "";
+   }
+
+
+   public void commaSeparated(int fromNum, int toNum) { //the tests will compare your printed output (not a returned string)
+       System.out.println();
+   }
+
+
+
+   public boolean isPalindrome(String myString) {
+       return false;
+   }
+
+
+   public void multiPrint(String toPrint, int num) { //the tests will compare your printed output (not a returned string)
+       System.out.println();
+   }
+}
